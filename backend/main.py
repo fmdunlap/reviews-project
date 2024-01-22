b527f64282ef57d524403030761d0f84c0b4d4df
Forrest Dunlap <forrestmdunlap@gmail.com>
2024-01-22 12:50:12 -0600

    CRITICAL FIX: Add emojis

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,19 @@
 from http.server import HTTPServer;
 from review_service_handler import ReviewServiceHandler;
+import multiprocessing
 
-PORT = 8000
+HTTP_PORT = 8000
+
+def start_http_server():
+    print(f"🚀 Starting server on port {HTTP_PORT}")
+    server = HTTPServer(("localhost", HTTP_PORT), ReviewServiceHandler)
+    server.serve_forever()
+
+def start_rss_polling_server():
+    print(f"📡 Starting RSS polling server")
+    pass
 
 if __name__ == "__main__":
-    server = HTTPServer(("localhost", PORT), ReviewServiceHandler)
-    server.serve_forever()
+    printf(f"🔥🔥 Starting Review Service 🔥🔥")
+    multiprocessing.Process(target=start_http_server).start()
+    multiprocessing.Process(target=start_rss_polling_server).start()
